a6b7b9ab1a99515f1059e6ebad8995761825a0c5
bunit17 <gitcommits@mattburnett.co.uk>
2016-06-11 17:45:31 +0100

    Adding camera entity

---
+++ src/AppBundle/Entity/camera.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of camera
+ *
+ *
+ * @author Matthew
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="camera")
+ * @ORM\HasLifecycleCallbacks
+ */
+
+class camera {
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $timestamp;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $count;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $running_count;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    protected $doors;
+
+    /**
+     * @ManyToOne(targetEntity="venue")
+     * @JoinColumn(name="venue_id", referencedColumnName="id")
+     */
+    protected $venue;
+
+
+
+
+
+    /**
+     * Set timestamp
+     *
+     * @param \DateTime $timestamp
+     * @ORM\PrePersist
+     * @return Example
+     */
+    public function setTimestamp()
+    {
+
+        if(!$this->timestamp){
+            $this->timestamp = new \DateTime();
+        }
+
+        return $this;
+    }
+
+
+}
