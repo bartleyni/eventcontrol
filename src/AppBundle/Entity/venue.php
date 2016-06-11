43cfdd964863abc6ed5c7ba20e6a6a49bde9a2f7
bunit17 <gitcommits@mattburnett.co.uk>
2016-06-11 17:54:46 +0100

    Adding camera entity

---
+++ src/AppBundle/Entity/venue.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of venue
+ *
+ *
+ * @author Matthew
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="venue")
+ */
+
+class venue {
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $name;
+
+     /**
+      * @OneToMany(targetEntity="camera", mappedBy="venue")
+     */
+    protected $cameras;
+
+
+
+
+
+
