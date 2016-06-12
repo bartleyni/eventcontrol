5f82c02d743bbcf79a217c7ff581fc5f44117dc4
bunit17 <gitcommits@mattburnett.co.uk>
2016-06-12 19:46:05 +0100

    Adding venuecamera

--- src/AppBundle/Entity/venue_cameraRepository.php
+++ src/AppBundle/Entity/venue_cameraRepository.php
@@ -12,7 +12,7 @@ public function getvenuecameras($id)
 return $this->getEntityManager()->createQuery('SELECT p.camera_id FROM AppBundle\Entity\venue_camera p  WHERE p.venue_id = :id')->setParameter('id', $id)->getResult();
 }
 
-public function getcameravenue($id)
+public function findOneByCameraId($id)
 {
     return $this->getEntityManager()->createQuery('SELECT p.venue_id FROM AppBundle\Entity\venue_camera p  WHERE p.camera_id = :id  LIMIT 1')->setParameter('id', $id)->getOneOrNullResult();
 }
