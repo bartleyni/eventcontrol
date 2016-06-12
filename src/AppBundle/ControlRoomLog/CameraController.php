3b1e4745eb1b0abc7e760995faedb7bb489dd016
bunit17 <gitcommits@mattburnett.co.uk>
2016-06-12 21:32:09 +0100

    Adding venuecamera

--- src/AppBundle/ControlRoomLog/CameraController.php
+++ src/AppBundle/ControlRoomLog/CameraController.php
@@ -26,7 +26,7 @@ class CameraController extends Controller
             $camera->setCountIn($count_in);
             $camera->setCountOut($count_out);
 
-            //print_r($em->getRepository('AppBundle\Entity\venue_camera')->findOneByCameraId($camera_id));
+            print_r($em->getRepository('AppBundle\Entity\venue_camera')->findOneByCameraId($camera_id));
             //$venue = new venue();
             //$venue->setName("test")'
             $venue = $em->getRepository('AppBundle\Entity\venue')->findOneBy(array('id' => $venue_id));
