c3ce0f9e9d4fef1a01ac9a6803be70d4f9fd9d4e
bunit17 <gitcommits@mattburnett.co.uk>
2016-06-12 16:29:53 +0100

    Addint prepersit to entity

--- src/AppBundle/Entity/camera.php
+++ src/AppBundle/Entity/camera.php
@@ -80,7 +80,7 @@ class camera {
      */
     public function calculate_running_total()
     {
-        $Camera = $em->createQueryBuilder()->select('count_in')
+        $Camera = $event->getEntityManager()->createQueryBuilder()->select('count_in')
             ->from("\Application\Entity\Camera", 'Camera')
             ->orderBy('Camera.id', 'DESC')
             ->setMaxResults(1)
