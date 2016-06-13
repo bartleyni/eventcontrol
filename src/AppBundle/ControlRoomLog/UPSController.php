5535f3e11d010b861748b2b6582043e078238777
Nick Bartley <github@nb221.com>
2016-06-13 17:47:56 +0100

    Debugging UPS Controller

--- src/AppBundle/ControlRoomLog/UPSController.php
+++ src/AppBundle/ControlRoomLog/UPSController.php
@@ -33,7 +33,7 @@ class UPSController extends Controller
         $qb = $em->createQueryBuilder(); 
         
         $qb
-            ->select('status.id, status.status, status.UPS, status.timestamp')
+            ->select('status.id, status.status, status.timestamp')
             ->from('AppBundle\Entity\UPS_Status', 'status')
             //->leftJoin('AppBundle\Entity\UPS', 'ups', 'WITH', 'ups.id = status.UPS')
             //->orderBy('ups.id', 'ASC')
