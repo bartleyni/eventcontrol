b46545c70c9491fb3cbc27446bd2e2870de1ddb0
Nick Bartley <github@nb221.com>
2016-06-13 17:40:03 +0100

    Added UPS Controller

---
+++ src/AppBundle/ControlRoomLog/UPSController.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\ControlRoomLog;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\UPS;
+use AppBundle\Entity\UPS_Status;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Description of UPSController
+ *
+ * @author Nick
+ */
+class UPSController extends Controller
+{
+    /**
+     * @Route("/UPS/status", name="UPS_status");
+     * 
+     */
+    public function UPSAction(Request $request)
+    {
+        $qb
+            ->select('status.id, status.status, status.UPS, status.timestamp, ups.name, ups.id, ups.location, ups.power')
+            ->from('AppBundle\Entity\UPS_Status', 'status')
+            ->leftJoin('AppBundle\Entity\UPS', 'ups', 'WITH', 'ups.id = status.UPS')
+            ->orderBy('ups.id', 'ASC')
+            ;
+        
+        $query = $qb->getQuery();
+        $ups_statuses = $query->getResult();
+        
+        if ($ups_statuses)
+            {
+            $response = new JsonResponse();
+            $response->setData(array(
+                'data' => $ups_statuses
+            ));
+
+            return $response;
+        }
+    }
+}
