5e15020926a58024d629a313e3f1b5183ed2b44a
Nick Bartley <github@nb221.com>
2016-06-13 20:03:13 +0100

    debugging ups json

--- src/AppBundle/ControlRoomLog/UPSController.php
+++ src/AppBundle/ControlRoomLog/UPSController.php
@@ -34,7 +34,7 @@ class UPSController extends Controller
         $qb = $em->createQueryBuilder(); 
         
         $qb
-            ->select('status.id, status.status, status.timestamp, ups.name, ups.location, ups.power, ups.id')
+            ->select('status, ups')
             ->from('AppBundle\Entity\UPS_Status', 'status')
             ->leftJoin('AppBundle\Entity\UPS', 'ups', 'WITH', 'ups.id = status.UPS')
             ->orderBy('ups.id', 'ASC')
