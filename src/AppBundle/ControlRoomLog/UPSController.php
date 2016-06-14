cd637a06705ee8ffde9aaf848dfc26b95ca881e1
Nick Bartley <github@nb221.com>
2016-06-14 23:50:18 +0100

    Re-enabled sidebar polling

--- src/AppBundle/ControlRoomLog/UPSController.php
+++ src/AppBundle/ControlRoomLog/UPSController.php
@@ -32,29 +32,8 @@ class UPSController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         
-        $upsi = $em->getRepository('AppBundle\Entity\UPS');
-        
-        $qb = $em->createQueryBuilder();
-        
-        //$current_data = $this->getEntityManager()->createQuery('SELECT p.id, p.timestamp, p.count_in, p.running_count_in, p.count_out, p.running_count_out, p.doors, p.camera_id FROM AppBundle\Entity\camera p  WHERE p.camera_id = :id ORDER BY p.timestamp DESC')->setParameter('id', $id)->setMaxResults(1)->getOneOrNullResult();
-
         $query = $em->createQuery('SELECT status1.id, status1.status, status1.timestamp, ups.name, ups.location, ups.power, ups.id FROM AppBundle\Entity\UPS_Status status1 JOIN status1.UPS ups WHERE status1.timestamp=(SELECT MAX(status2.timestamp) FROM AppBundle\Entity\UPS_Status status2 WHERE status1.UPS=status2.UPS)');
-                
-        $qb
-            ->select('ups.id, status.timestamp, status.status, ups.name, ups.location, ups.power')
-            ->from('AppBundle\Entity\UPS_Status', 'status')
-            ->leftJoin('AppBundle\Entity\UPS', 'ups', 'WITH', 'ups.id = status.UPS')
-            //->orderBy('status.timestamp', 'DESC')
-            //->leftJoin('AppBundle\Entity\UPS_Status', 'status2', 'WITH', 'status.id = status2.id')
-            //->where('status.timestamp < status2.timestamp')
-            //->andWhere('status2.timestamp is NULL')
-            //->orderBy('ups.id', 'ASC')
-            ->groupBy('ups.id')
-            //->addOrderBy('status2.timestamp', 'DESC')
-            ;
 
-        //$query = $qb->getQuery();
-         
         $ups_statuses = $query->getResult();
         
         if ($ups_statuses)
