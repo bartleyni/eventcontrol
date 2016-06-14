d750910e84b8993979ed6d5576efa021cc54939a
Nick Bartley <github@nb221.com>
2016-06-14 23:38:23 +0100

    UPSController debug

--- src/AppBundle/ControlRoomLog/UPSController.php
+++ src/AppBundle/ControlRoomLog/UPSController.php
@@ -38,7 +38,7 @@ class UPSController extends Controller
         
         //$current_data = $this->getEntityManager()->createQuery('SELECT p.id, p.timestamp, p.count_in, p.running_count_in, p.count_out, p.running_count_out, p.doors, p.camera_id FROM AppBundle\Entity\camera p  WHERE p.camera_id = :id ORDER BY p.timestamp DESC')->setParameter('id', $id)->setMaxResults(1)->getOneOrNullResult();
 
-        $query = $em->createQuery('SELECT status1 FROM AppBundle\Entity\UPS_Status status1 WHERE status1.timestamp=(SELECT MAX(status2.timestamp) FROM AppBundle\Entity\UPS_Status status2 WHERE status1.UPS_id=status2.UPS_id)');
+        $query = $em->createQuery('SELECT status1 FROM AppBundle\Entity\UPS_Status status1 WHERE status1.timestamp=(SELECT MAX(status2.timestamp) FROM AppBundle\Entity\UPS_Status status2 WHERE status1.UPS=status2.UPS)');
                 
         $qb
             ->select('ups.id, status.timestamp, status.status, ups.name, ups.location, ups.power')
