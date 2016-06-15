e01b883f0bf1c248e3d23b524cc217e440f21a53
bunit17 <gitcommits@mattburnett.co.uk>
2016-06-15 12:34:01 +0100

    Adding venuecamera

---
+++ src/AppBundle/ControlRoomLog/VenueController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\ControlRoomLog;
+use AppBundle\Entity\venue;
+use AppBundle\Entity\camera;
+use AppBundle\Entity\venue_camera;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\Query\ResultSetMapping;
+
+class VenueController extends Controller
+{
+        /**
+     * @Route("/venue/jsondata", name="People counting json data");
+     *
+     */
+    public function json_data(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery("SELECT venue FROM AppBundle\Entity\venue venue");
+
+        $data = $query->getArrayResult();
+
+        print_r($data);
+        if ($data)
+        {
+            $response = new JsonResponse();
+            $response->setData($data);
+
+        } else {
+            $response->setContent('Hello World');
+            $response->headers->set('Content-Type', 'text/plain');
+            $response->setStatusCode(Response::HTTP_NOT_FOUND);
+        }
+
+        return $response;
+
+
+        //return $response;
+    }
+
+}
