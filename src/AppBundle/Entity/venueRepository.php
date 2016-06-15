8a6889d78c8b5be3bf8e497f9cf46aa59d38a8b2
bunit17 <gitcommits@mattburnett.co.uk>
2016-06-15 11:20:04 +0100

    Adding venuecamera

--- src/AppBundle/Entity/venueRepository.php
+++ src/AppBundle/Entity/venueRepository.php
@@ -12,11 +12,19 @@ class venueRepository extends EntityRepository
     {
        
         $cameras = $this->getEntityManager()->getRepository('AppBundle\Entity\venue_camera')->getvenuecameras($id);
+        $output = array();
+        foreach ($cameras as $camera) {
 
+            $camera_count = $this->getEntityManager()->getRepository('AppBundle\Entity\venue_camera')->getcameracount($camera['camera_id']);
+            print_r($camera['camera_id']);
+            print_r($camera_count);
+            $output['running_count_in'] += $camera_count['running_count_in'];
+            $output['running_count_out'] += $camera_count['running_count_out'];
+        }
         //$output['running_count_in']=$current_data[running_count_in]-$doors_data[running_count_in];
         //$output['running_count_out']=$current_data[running_count_out]-$doors_data[running_count_out];
 
-        return $cameras;
+        return $output;
 
     }
 }
