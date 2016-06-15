0e5a4b122a60ee3d7e9762eea1cdf1d514a7ff1a
bunit17 <gitcommits@mattburnett.co.uk>
2016-06-15 11:06:03 +0100

    Adding venuecamera

--- src/AppBundle/ControlRoomLog/CameraController.php
+++ src/AppBundle/ControlRoomLog/CameraController.php
@@ -28,21 +28,21 @@ class CameraController extends Controller
 
             $venue = $em->getRepository('AppBundle\Entity\venue_camera')->getcameravenue($camera_id);
             print_r($venue);
-            //echo $venue['1'];
-            //print_r($em->getRepository('AppBundle\Entity\venue_camera')->getvenuecameras($venue['1']));
-            //print_r($em->getRepository('AppBundle\Entity\venue')->getvenuecount($venue['1']));
+            echo $venue[1];
+            print_r($em->getRepository('AppBundle\Entity\venue_camera')->getvenuecameras($venue[1]));
+            print_r($em->getRepository('AppBundle\Entity\venue')->getvenuecount($venue[1]));
             //$venue = new venue();
             //$venue->setName("test")'
-           // $venue = $em->getRepository('AppBundle\Entity\venue')->findOneBy(array('id' => $venue['1']));
+            $venue = $em->getRepository('AppBundle\Entity\venue')->findOneBy(array('id' => $venue[1]));
             
-            //$camera->setVenue($venue);
+            $camera->setVenue($venue);
 
             //$venue = new venue();
             //$venue->getId($venue_id);
             //$camera->setVenue($venue);
             
-            ///$em->persist($camera);
-            //$em->flush();
+            $em->persist($camera);
+            $em->flush();
 
             //return new Response('Saved new camera data with id '.$camera->getId());
         }
