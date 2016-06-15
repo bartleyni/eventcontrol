ea37c103b5e082b3687fd3b76b145c6d99acfa9d
bunit17 <gitcommits@mattburnett.co.uk>
2016-06-15 20:53:11 +0100

    Adding venue skew

--- src/AppBundle/Entity/venueRepository.php
+++ src/AppBundle/Entity/venueRepository.php
@@ -32,21 +32,21 @@ class venueRepository extends EntityRepository
 
                 $camera_doors = $this->getEntityManager()->getRepository('AppBundle\Entity\camera')->getcameradoors($camera['camera_id'], $timestamp);
                 $camera_count = $this->getEntityManager()->getRepository('AppBundle\Entity\camera')->getcameracount($camera['camera_id']);
-                echo "print camre doors";
-                print_r($camera_doors);
-                echo "print camera count";
-                print_r($camera_count);
+                //echo "print camre doors";
+                //print_r($camera_doors);
+                //echo "print camera count";
+                //print_r($camera_count);
                 $output['running_count_in'] += $camera_count['running_count_in'] - $camera_doors['running_count_in'];
                 $output['running_count_out'] += $camera_count['running_count_out'] - $camera_doors['running_count_out'];
             }
-            echo "print before output";
-            print_r($output);
+            //echo "print before output";
+            //print_r($output);
             foreach ($skews as $skew) {
                 $output['running_count_in'] += $skew['skew_in'];
                 $output['running_count_out'] += $skew['skew_out'];
             }
             echo "print after output";
-            print_r($output);
+            //print_r($output);
             //$output['running_count_in']=$current_data[running_count_in]-$doors_data[running_count_in];
             //$output['running_count_out']=$current_data[running_count_out]-$doors_data[running_count_out];
         }
