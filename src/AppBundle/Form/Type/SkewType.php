c0a62f20b4d626b004cd35928531d229c3d8ac2d
bunit17 <gitcommits@mattburnett.co.uk>
2016-06-15 21:59:30 +0100

    Adding venue skew

---
+++ src/AppBundle/Form/Type/SkewType.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntergerType;
+
+class SkewType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('skew_in', 'integer')
+            ->add('skew_out', 'integer');
+    }
+    public function getName()
+    {
+        return 'skew_form';
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array('data_class' => 'AppBundle\Entity\skew'));
+    }
+}
