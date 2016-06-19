025304bd72dcf7038f8fe6247c1cacdbd1d050ff
Nick Bartley <github@nb221.com>
2016-06-19 22:56:25 +0100

    Debugging timeout function

--- src/AppBundle/ControlRoomLog/UPSController.php
+++ src/AppBundle/ControlRoomLog/UPSController.php
@@ -42,8 +42,8 @@ class UPSController extends Controller
         
         foreach ($ups_statuses as $status)
         {
-            $interval1 = date_diff($status.timestamp, $now);
-            $interval2 = intval($interval1->format('%i'));
+            //$interval1 = date_diff($status.timestamp, $now);
+            //$interval2 = intval($interval1->format('%i'));
             if ($interval2 > 15)
             {
                 $status->setStatus('Timeout');
