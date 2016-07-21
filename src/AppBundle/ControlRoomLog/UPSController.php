ad9ea74a3a7aebaddadb9919fac17111beec67db
Nick Bartley <github@nb221.com>
2016-07-21 18:20:23 +0100

    changed UPS controller

--- src/AppBundle/ControlRoomLog/UPSController.php
+++ src/AppBundle/ControlRoomLog/UPSController.php
@@ -66,9 +66,9 @@ class UPSController extends Controller
         
         $UPSstatus->setUPS($ups);
         $UPSstatus->setStatus($status);
-        $UPSstatus->setLineVoltage(NULL);
-        $UPSstatus->setLoad(NULL);
-        $UPSstatus->setBatteryVoltage(NULL);  
+        $UPSstatus->setLineVoltage(0);
+        $UPSstatus->setLoad(0);
+        $UPSstatus->setBatteryVoltage(0);  
         $UPSstatus->setTimestamp();
         
         $em->persist($UPSstatus);
