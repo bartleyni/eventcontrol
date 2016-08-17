fb6ca4068890bfdb1e49cb939235501702bb591d
Nick Bartley <github@nb221.com>
2016-08-17 17:38:07 +0100

    Created user_events entity

---
+++ src/AppBundle/Entity/user_events.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Entity;
+
+/**
+ * Description of user_events
+ *
+ * @author Nick
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="user_events")
+ */
+
+class user_events {
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="User_id", referencedColumnName="id")
+     */
+    protected $User_id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="event")
+     * @ORM\JoinColumn(name="event_id", referencedColumnName="id")
+     */
+    protected $event_id;
+    
+}
