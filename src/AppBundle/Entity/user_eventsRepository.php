05e88f93dda5df5ec564ac2723d565fc41f3e5bc
Nick Bartley <github@nb221.com>
2016-08-22 20:46:15 +0100

    debug evnet statuts json

--- src/AppBundle/Entity/user_eventsRepository.php
+++ src/AppBundle/Entity/user_eventsRepository.php
@@ -9,11 +9,11 @@ class user_eventsRepository extends EntityRepository
 {
     public function getEventUsers($eventId)
     {
-        return $this->getEntityManager()->createQuery('SELECT user_event.User_id FROM AppBundle\Entity\user_events user_event WHERE user_event.event_id = :id')->setParameter('id', $eventId)->getResult();
+        return $this->getEntityManager()->createQuery('SELECT IDENTITY(user_event.User_id) FROM AppBundle\Entity\user_events user_event WHERE user_event.event_id = :id')->setParameter('id', $eventId)->getResult();
     }
     
     public function getEventUsersNotInList($userIdList)
     {
-        return $this->getEntityManager()->createQuery('SELECT user_event.User_id FROM AppBundle\Entity\user_events user_event WHERE user_event.event_id = :id AND user_event.User_id NOT IN (:users)')->setParameter('id', $eventId)->setParameter('users', $userIdList)->getResult();
+        return $this->getEntityManager()->createQuery('SELECT IDENTITY(user_event.User_id) FROM AppBundle\Entity\user_events user_event WHERE user_event.event_id = :id AND user_event.User_id NOT IN (:users)')->setParameter('id', $eventId)->setParameter('users', $userIdList)->getResult();
     }
 }
