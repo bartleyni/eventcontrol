0e5e512c4592c17a65215dc5191f104be8f3b613
Nick Bartley <github@nb221.com>
2016-08-22 20:57:09 +0100

    debugging

--- src/AppBundle/Entity/user_eventsRepository.php
+++ src/AppBundle/Entity/user_eventsRepository.php
@@ -14,6 +14,6 @@ class user_eventsRepository extends EntityRepository
     
     public function getEventUsersNotInList($eventId, $userIdList)
     {
-        return $this->getEntityManager()->createQuery('SELECT IDENTITY(user_event.User_id) FROM AppBundle\Entity\user_events user_event WHERE user_event.event_id = :id AND user_event.User_id NOT IN (:users)')->setParameter('id', $eventId)->setParameter('users', $userIdList)->getResult();
+        return $this->getEntityManager()->createQuery('SELECT user_event FROM AppBundle\Entity\user_events user_event WHERE user_event.event_id = :id AND user_event.User_id NOT IN (:users)')->setParameter('id', $eventId)->setParameter('users', $userIdList)->getResult();
     }
 }
