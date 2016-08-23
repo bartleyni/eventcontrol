7ded11af04aa9932682ab67604f817bb3d60d8ae
Nick Bartley <github@nb221.com>
2016-08-23 07:28:30 +0100

    Update user_eventsRepository.php

--- src/AppBundle/Entity/user_eventsRepository.php
+++ src/AppBundle/Entity/user_eventsRepository.php
@@ -16,4 +16,15 @@ class user_eventsRepository extends EntityRepository
     {
         return $this->getEntityManager()->createQuery('SELECT user_event FROM AppBundle\Entity\user_events user_event WHERE user_event.event_id = :id AND user_event.User_id NOT IN (:users)')->setParameter('id', $eventId)->setParameter('users', $userIdList)->getResult();
     }
+    
+    public function getActiveEvent($userId)
+    {
+        $now = new \DateTime();
+        
+        //$active_event = $this->getEntityManager()->createQuery('SELECT event.id FROM AppBundle\Entity\event event WHERE event.date1 = :date2 AND event.date2 = :date1')->setParameter('date1', $date)->setParameter('date2', $date)->getResult();
+    
+        
+        return $this->getEntityManager()->createQuery('SELECT user_event.event_id FROM AppBundle\Entity\user_events user_event WHERE user_event.User_id = :id AND :nowdate BETWEEN event.event_log_start_date AND event.event_log_stop_date')->setParameter('id', $userId)->setParameter('nowdate', $now)->getResult();
+    }
+}
 }
