e0ebf7941e5259b1ab43d785a11a5f7c666d7c36
Nick Bartley <github@nb221.com>
2016-08-23 08:38:03 +0100

    Update entry_form.php

--- src/AppBundle/ControlRoomLog/entry_form.php
+++ src/AppBundle/ControlRoomLog/entry_form.php
@@ -34,7 +34,8 @@ class entry_form extends Controller
         $usr = $this->get('security.context')->getToken()->getUser();
         $operatorId = $usr->getId();
         
-        $user_event = $em->getRepository('AppBundle\Entity\user_events')->findOneBy(array('User_id' => $operatorId, 'active' => true));
+        //$user_event = $em->getRepository('AppBundle\Entity\user_events')->findOneBy(array('User_id' => $operatorId, 'active' => true));
+        $event = $em->getRepository('AppBundle\Entity\user_events')->getActiveEvent($operatorId);
         $em->flush();
         
         if($user_event)
