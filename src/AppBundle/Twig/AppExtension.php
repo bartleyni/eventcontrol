e06ff82c2701a65afe43dc4536b687937cbb7085
Nick Bartley <github@nb221.com>
2016-08-24 10:00:17 +0100

    Update AppExtension.php

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -213,22 +213,23 @@ class AppExtension extends \Twig_Extension
             ->leftJoin('AppBundle\Entity\medical_log', 'med', 'WITH', 'med.log_entry_id = entry.id')
             ->leftJoin('AppBundle\Entity\lost_property', 'lost', 'WITH', 'lost.log_entry_id = entry.id')
             ->leftJoin('AppBundle\Entity\event', 'event', 'WITH', 'event.id = entry.event')
-            ->where($qb->expr()->andX(
-                        $qb->expr()->isNotNull('med.medical_description'),
-                        $qb->expr()->isNull('med.medical_entry_closed_time')
-                    ))
-            ->where($qb->expr()->andX(
-                            $qb->expr()->isNotNull('sec.security_description'),
-                            $qb->expr()->isNull('sec.security_entry_closed_time')
-                    ))
-            ->where($qb->expr()->andX(
+            ->where($qb->expr()->orX(
+                        $qb->expr()->andX(
                             $qb->expr()->isNotNull('gen.general_description'),
                             $qb->expr()->isNull('gen.general_entry_closed_time')
-                    ))
-            ->where($qb->expr()->andX(
+                             ),                            
+                        $qb->expr()->andX(
+                            $qb->expr()->isNotNull('sec.security_description'),
+                            $qb->expr()->isNull('sec.security_entry_closed_time')
+                             ),
+                        $qb->expr()->andX(
+                            $qb->expr()->isNotNull('med.medical_description'),
+                            $qb->expr()->isNull('med.medical_entry_closed_time')
+                            ),
+                        $qb->expr()->andX(
                             $qb->expr()->isNotNull('lost.lost_property_description'),
                             $qb->expr()->isNull('lost.lost_property_entry_closed_time')
-                    ))
+                            )))
             ->andWhere('event.id = :eventId')
             ->setParameter('eventId', $this->getEventById($operatorId))
             ;
