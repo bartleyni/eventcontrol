1b489a35c84b43f4de5fb41570a90b43222def90
Nick Bartley <github@nb221.com>
2016-09-07 19:36:55 +0100

    Weather javascript

--- src/AppBundle/ControlRoomLog/EventController.php
+++ src/AppBundle/ControlRoomLog/EventController.php
@@ -226,9 +226,6 @@ class EventController extends Controller
             throw $this->createAccessDeniedException();
         }
         
-        //$parametersAsArray = [];
-        //$content = http_get("https://api.forecast.io/forecast/9c4ec6b414ca6374999b6b88fbc44634/51.379551,-2.325717");
-        
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'https://api.forecast.io/forecast/9c4ec6b414ca6374999b6b88fbc44634/51.379551,-2.325717');
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
