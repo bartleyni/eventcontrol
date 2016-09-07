2a0e8d429e8f3a5396128cd6d537709395f802db
Nick Bartley <github@nb221.com>
2016-09-07 19:29:46 +0100

    weather debug

--- src/AppBundle/ControlRoomLog/EventController.php
+++ src/AppBundle/ControlRoomLog/EventController.php
@@ -238,7 +238,10 @@ class EventController extends Controller
 
         if ($content){
             $data = json_decode($content, true);
-            $summary = $data['hourly']['summary'];
+            $summary = $data['minutely']['summary'];
+            if (!$summary){
+                $summary = $data['hourly']['summary'];
+            }
         }
         
         $response = new Response($summary,Response::HTTP_OK, array('content-type' => 'text/html'));
