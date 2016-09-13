9ad9562ec217fd5ce3627b611219ad8ac1a5fd25
Nick Bartley <github@nb221.com>
2016-09-13 14:11:40 +0100

    Update EventController.php

--- src/AppBundle/ControlRoomLog/EventController.php
+++ src/AppBundle/ControlRoomLog/EventController.php
@@ -41,21 +41,21 @@ class EventController extends Controller
             ;
         
         $query1 = $qb1->getQuery();
-        $events = $query1->getResult();
+        $events1 = $query1->getResult();
         
         $qb2 = $em->createQueryBuilder(); 
         
         $qb2
             ->select('event.id, event.client, event.name, event.event_date, event.event_log_start_date, event.event_log_stop_date, event.event_report_filename, event.event_report_run_date')
             ->from('AppBundle\Entity\event', 'event')
-            //->where('event.event_date < :nowDate')
-            //->setParameter('nowDate', $now)
+            ->where('event.event_date < :nowDate')
+            ->setParameter('nowDate', $now)
             ->orderBy('event.event_date', 'DESC')
             ;
         
         $query2 = $qb2->getQuery();
         $events2= $query2->getResult();
-        //$events[] = $events2;
+        $events = array_merge($events1,$events2); 
         return $this->render('eventList.html.twig', array('events' => $events));
     }
 
