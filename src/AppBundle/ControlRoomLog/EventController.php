537853475b8504d2882b4ecff6c93e0847b3cb90
Nick Bartley <github@nb221.com>
2016-09-23 11:58:51 +0100

    Update EventController.php

--- src/AppBundle/ControlRoomLog/EventController.php
+++ src/AppBundle/ControlRoomLog/EventController.php
@@ -383,7 +383,7 @@ class EventController extends Controller
         $usr = $this->get('security.context')->getToken()->getUser();
         $operatorId = $usr->getId();
         
-        $iframe = '<iframe id="iframe_radar" name="iframe_radar" src="http://premium.raintoday.co.uk/mobile" frameborder=0 scrolling=no height="600px" class="col-md-12 embed-responsive-item" ></iframe>';
+        $iframe = '<iframe id="iframe_radar" name="iframe_radar" src="//premium.raintoday.co.uk/mobile" frameborder=0 scrolling=no height="600px" class="col-md-12 embed-responsive-item" ></iframe>';
         $data = "u: weather@nb221.com p: uM7qflPqD91W";
         $username = "weather@nb221.com";
         $password = "uM7qflPqD91W";
