eaad2fae1d4b9ed18743888c33c86213196d024a
Nick Bartley <github@nb221.com>
2016-09-28 01:35:27 +0100

    Updated indexes

--- src/AppBundle/Entity/UPS_Status.php
+++ src/AppBundle/Entity/UPS_Status.php
@@ -18,7 +18,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="UPS_Status", indexes={@ORM\Index(name="ups_id_idx", columns={"UPS_id"})})
+ * @ORM\Table(name="UPS_Status", indexes={@ORM\Index(name="ups_id_timestamp_idx", columns={"UPS_id", "timestamp"})})
  * @ORM\Entity(repositoryClass="AppBundle\Entity\UPS_StatusRepository")
  */
 class UPS_Status {
