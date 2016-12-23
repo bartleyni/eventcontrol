25281b53939f109b54cc19760fd0b0316fee0f93
bunit17 <gitcommits@mattburnett.co.uk>
2016-12-23 23:06:00 +0000

    Rename camera to camera_count

--- src/AppBundle/Entity/camera_countRepository.php
+++ src/AppBundle/Entity/camera_countRepository.php
@@ -19,8 +19,8 @@ class camera_countRepository extends EntityRepository
     {
         $current_data = $this->getEntityManager()->createQuery('SELECT p.id, p.timestamp, p.count_in, p.running_count_in, p.count_out, p.running_count_out, p.camera_id FROM AppBundle\Entity\camera_count p  WHERE p.camera_id = :id ORDER BY p.timestamp DESC')->setParameter('id', $id)->setMaxResults(1)->getOneOrNullResult();
 
-        $output['running_count_in']=$current_data[running_count_in];
-        $output['running_count_out']=$current_data[running_count_out];
+        $output['running_count_in']=$current_data['running_count_in'];
+        $output['running_count_out']=$current_data['running_count_out'];
       
         return $output;
 
