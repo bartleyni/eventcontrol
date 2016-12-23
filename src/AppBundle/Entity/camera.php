42c3fb475960ad85e1fa5e18cc156ad9a5accf1e
bunit17 <gitcommits@mattburnett.co.uk>
2016-12-23 22:46:36 +0000

    Rename camera to camera_count

---
+++ src/AppBundle/Entity/camera.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of camera
+ *
+ *
+ * @author Matthew
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\cameraRepository")
+ * @ORM\Table(name="camera")
+ * @ORM\HasLifecycleCallbacks
+ */
+
+class camera {
+
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    protected $location;
+}
--- src/AppBundle/Entity/cameraRepository.php
+++ src/AppBundle/Entity/cameraRepository.php
@@ -6,42 +6,10 @@ use AppBundle\Entity;
 use Doctrine\ORM\EntityRepository;
 
 
-class camera_countRepository extends EntityRepository
+class cameraRepository extends EntityRepository
 {
 
-    public function getcameradoors($id, $timestamp)
-    {
-        //return "hi";
-        return $this->getEntityManager()->createQuery('SELECT p.id, p.timestamp, p.count_in, p.running_count_in, p.count_out, p.running_count_out, p.camera_id FROM AppBundle\Entity\camera_count p  WHERE p.camera_id = :id AND p.timestamp <= :timestamp ORDER BY p.timestamp DESC ')->setParameter('id', $id)->setParameter('timestamp', $timestamp)->setMaxResults(1)->getOneOrNullResult();
-    }
-
-    public function getcameracount($id)
-    {
-        $current_data = $this->getEntityManager()->createQuery('SELECT p.id, p.timestamp, p.count_in, p.running_count_in, p.count_out, p.running_count_out, p.camera_id FROM AppBundle\Entity\camera_count p  WHERE p.camera_id = :id ORDER BY p.timestamp DESC')->setParameter('id', $id)->setMaxResults(1)->getOneOrNullResult();
-
-        $output['running_count_in']=$current_data[running_count_in];
-        $output['running_count_out']=$current_data[running_count_out];
-      
-        return $output;
-
-    }
-    public function iscamerauptodate($id)
-    {
-        $date = new \DateTime();
-        $date->modify('-1 minutes');
-        $current_data = $this->getEntityManager()->createQuery('SELECT p.id, p.timestamp, p.count_in, p.running_count_in, p.count_out, p.running_count_out, p.camera_id FROM AppBundle\Entity\camera_count p  WHERE p.camera_id = :id AND p.timestamp >= :date  ORDER BY p.timestamp DESC')->setParameter('id', $id)->setParameter('date', $date)->setMaxResults(1)->getResult();
-        if(count($current_data)>0){
-            return true;
-        }else{
-            return false;
-        }
-       
-        //$output['running_count_in']=$current_data[running_count_in];
-        //$output['running_count_out']=$current_data[running_count_out];
-
-        //return $output;
-
-    }
+   
     
     
 }
