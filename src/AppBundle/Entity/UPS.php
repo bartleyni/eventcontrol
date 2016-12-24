20983d12b5549588ade23380fb42501c1c246ff2
Nick Bartley <github@nb221.com>
2016-12-24 00:22:17 +0000

    Modified event schema

--- src/AppBundle/Entity/UPS.php
+++ src/AppBundle/Entity/UPS.php
@@ -51,7 +51,7 @@ class UPS {
     private $UPS_Status;
     
     /**
-     * @ORM\ManyToMany(targetEntity="event", inversedBy="UPS")
+     * @ORM\ManyToMany(targetEntity="event", inversedBy="UPS", mappedBy="event")
      * @ORM\JoinColumn(name="event_id", referencedColumnName="id", onDelete="CASCADE")
      */
     private $event;
