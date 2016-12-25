91b96fdbe032aa8c8d2aa9627053649c27d0477e
Nick Bartley <github@nb221.com>
2016-12-25 00:25:58 +0000

    fixing event controller error

--- src/AppBundle/ControlRoomLog/EventController.php
+++ src/AppBundle/ControlRoomLog/EventController.php
@@ -145,14 +145,15 @@ class EventController extends Controller
             $query = $qb->getQuery();
             $operators = $query->getResult();
             
-            if (!$form->isSubmitted() or !$form->isValid()){
-                if($event->getOverlayImage()){
-                    $event->setOverlayImage(
-                        new File($this->getParameter('overlay_directory').'/'.$event->getOverlayImage())
-                    );
+            if (!isset($form)){
+                if (!$form->isSubmitted() or !$form->isValid()){
+                    if($event->getOverlayImage()){
+                        $event->setOverlayImage(
+                            new File($this->getParameter('overlay_directory').'/'.$event->getOverlayImage())
+                        );
+                    }
                 }
             }
-            
             $em->flush();
             $form = $this->createForm(new EventType($this->getDoctrine()->getManager()), $event, array('event_id' => $editId,));
             $form->handleRequest($request);
