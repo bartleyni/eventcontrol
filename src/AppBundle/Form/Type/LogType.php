fcea39958719ee711c94044218669f72f7f6b567
Nick Bartley <github@nb221.com>
2016-12-26 13:59:30 +0000

    debugging map layout

--- src/AppBundle/Form/Type/LogType.php
+++ src/AppBundle/Form/Type/LogType.php
@@ -45,7 +45,7 @@ class LogType extends AbstractType
                 'label' => 'Short Description',
                 'attr' => array(
                     'class' => 'form-control',
-                    'rows' => '3'
+                    'rows' => '4'
                 )
             ))
             ->add('location', 'text', array(
