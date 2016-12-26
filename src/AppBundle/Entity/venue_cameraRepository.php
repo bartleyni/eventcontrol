09a4a40a1d6094ebd414f2e9f3bb00eb8672e23c
bunit17 <gitcommits@mattburnett.co.uk>
2016-12-26 21:38:28 +0000

    Venue_camera assosiation

--- src/AppBundle/Entity/venue_cameraRepository.php
+++ src/AppBundle/Entity/venue_cameraRepository.php
@@ -6,10 +6,10 @@ class venue_cameraRepository extends EntityRepository
 {
     public function getvenuecameras($id)
     {
-        return $this->getEntityManager()->createQuery('SELECT p.camera, p.inverse FROM AppBundle\Entity\venue_camera p  WHERE p.venue = :id')->setParameter('id', $id)->getResult();
+        return $this->getEntityManager()->createQuery('SELECT (p.camera_id) as camera, p.inverse FROM AppBundle\Entity\venue_camera p  WHERE p.venue = :id')->setParameter('id', $id)->getResult();
     }
     public function getcameravenue($id)
     {
-        return $this->getEntityManager()->createQuery('SELECT IDENTITY(p.venue) as venue FROM AppBundle\Entity\venue_camera p  WHERE p.camera = :id')->setParameter('id', $id)->getResult();
+        return $this->getEntityManager()->createQuery('SELECT IDENTITY(p.venue_id) as venue FROM AppBundle\Entity\venue_camera p  WHERE p.camera = :id')->setParameter('id', $id)->getResult();
     }
 }
