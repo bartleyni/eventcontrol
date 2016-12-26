629e11e1e4fe2ac93dab0d7e767e07d24a05e028
bunit17 <gitcommits@mattburnett.co.uk>
2016-12-26 23:00:08 +0000

    Camera and Camer_count association

--- src/AppBundle/Entity/venue_cameraRepository.php
+++ src/AppBundle/Entity/venue_cameraRepository.php
@@ -6,7 +6,7 @@ class venue_cameraRepository extends EntityRepository
 {
     public function getvenuecameras($id)
     {
-        return $this->getEntityManager()->createQuery('SELECT (p.camera_id) as camera_id, p.inverse FROM AppBundle\Entity\venue_camera p  WHERE p.venue_id = :id')->setParameter('id', $id)->getResult();
+        return $this->getEntityManager()->createQuery('SELECT IDENTITY(p.camera_id) as camera_id, p.inverse FROM AppBundle\Entity\venue_camera p  WHERE p.venue_id = :id')->setParameter('id', $id)->getResult();
     }
     public function getcameravenue($id)
     {
