6a574c3e50dfc637e2f6609776bc52343eb1d320
Nick Bartley <github@nb221.com>
2016-12-26 21:00:18 +0000

    adjusting collection form

--- src/AppBundle/Form/Type/LocationType.php
+++ src/AppBundle/Form/Type/LocationType.php
@@ -23,13 +23,13 @@ class LocationType extends AbstractType
     {
         $builder
             ->add('locationText', 'text', array(
-                'label' => 'Location (Text):',
+                'label' => 'Location (Text): ',
                 'attr' => array(
                     'class' => 'collection'
                 )
             ))
             ->add('locationLatLong', 'text', array(
-                'label' => 'Co-ordinates (Latitude, Longitude):',
+                'label' => 'Co-ordinates (Latitude, Longitude): ',
                 'attr' => array(
                     'class' => 'collection'
                 )
