443e8be3019567d7c408133894857ee1b361f226
Nick Bartley <github@nb221.com>
2016-12-27 01:26:37 +0000

    Updated placeholder location

--- src/AppBundle/Form/Type/LocationType.php
+++ src/AppBundle/Form/Type/LocationType.php
@@ -24,15 +24,15 @@ class LocationType extends AbstractType
         $builder
             ->add('locationText', 'text', array(
                 'label' => 'Location (Text): ',
-                'placeholder' => 'Location',
                 'attr' => array(
-                    'class' => 'form-control'
+                    'class' => 'form-control',
+                    'placeholder' => 'Location'
                 )
             ))
             ->add('locationLatLong', 'text', array(
                 'label' => 'Co-ordinates (Latitude, Longitude): ',
-                'placeholder' => 'Co-ordinates (Latitude, Longitude)',
                 'attr' => array(
+                    'placeholder' => 'Co-ordinates (Latitude, Longitude)',
                     'class' => 'form-control'
                 )
             ))    
