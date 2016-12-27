65a9a47b1d00e96a5d23f822d4bf6a827ffb7867
Nick Bartley <github@nb221.com>
2016-12-27 21:15:01 +0000

    Modified entry form

--- src/AppBundle/ControlRoomLog/entry_form.php
+++ src/AppBundle/ControlRoomLog/entry_form.php
@@ -64,13 +64,14 @@ class entry_form extends Controller
                 $new_entry->setEvent($event);
                 
                 //Lookup log location in event location entity and if latlong is blank use data
-                if($form['longitude']->getData() == null){
-                    $log_location = $form['location']->getData();
-                    //$location = $em->getRepository('AppBundle\Entity\Locations')->findOneBy(array('event' => $event, 'locationText' => $log_location));
-                    //$latLong = explode(" ,", $location->getLocationLatLong());
-                    //$new_entry->setLatitude($latLong[0]);
-                    //$new_entry->setLongitude($latLong[1]);
-                }
+                $variable = $form['longitude']->getData();
+//                if($form['longitude']->getData() == null){
+//                    $log_location = $form['location']->getData();
+//                    $location = $em->getRepository('AppBundle\Entity\Locations')->findOneBy(array('event' => $event, 'locationText' => $log_location));
+//                    $latLong = explode(" ,", $location->getLocationLatLong());
+//                    $new_entry->setLatitude($latLong[0]);
+//                    $new_entry->setLongitude($latLong[1]);
+//                }
                 $em->persist($new_entry);
                 $em->flush();
                 return $this->redirect($this->generateUrl('edit_entry', array('id' => $new_entry->getId())));
