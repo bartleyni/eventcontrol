78729e6a4c207238506e512fef0cc7d06c3e187d
Nick Bartley <github@nb221.com>
2016-12-27 20:38:28 +0000

    lookup mods

--- src/AppBundle/ControlRoomLog/edit_entry_form.php
+++ src/AppBundle/ControlRoomLog/edit_entry_form.php
@@ -268,7 +268,8 @@ class edit_entry_form extends Controller
             array_push($lookup1, $lookupLocation);
         }
         
-        $lookup = array_unique($lookup1, SORT_REGULAR);
+        //$lookup = array_unique($lookup1, SORT_REGULAR);
+        $lookup = $lookup1;
         
         $response = new JsonResponse();
         $response->setData($lookup);
