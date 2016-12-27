a9757f425519bb81907130c1aaba3a3fd9981117
Nick Bartley <github@nb221.com>
2016-12-27 18:34:22 +0000

    debugging locaito query

--- src/AppBundle/Entity/locationsRepository.php
+++ src/AppBundle/Entity/locationsRepository.php
@@ -22,9 +22,9 @@ class locationsRepository extends EntityRepository
     public function getEventLocationLookup($event, $location=null)
     {
         if ($location){
-            return $this->getEntityManager()->createQuery('SELECT DISTINCT location.locationText as location FROM AppBundle\Entity\Locations location WHERE location.event = :event AND location.locationText LIKE :location')->setParameter('event', $event)->setParameter('location', '%'.$location.'%')->getResult();
+            return $this->getEntityManager()->createQuery('SELECT DISTINCT locations.locationText as location FROM AppBundle\Entity\Locations locations WHERE locations.event = :event AND locations.locationText LIKE :locationIn')->setParameter('event', $event)->setParameter('locationIn', '%'.$location.'%')->getResult();
         } else {
-            return $this->getEntityManager()->createQuery('SELECT DISTINCT location.locationText as location FROM AppBundle\Entity\Locations location WHERE location.event = :event')->setParameter('event', $event)->getResult();
+            return $this->getEntityManager()->createQuery('SELECT DISTINCT locations.locationText as location FROM AppBundle\Entity\Locations locations WHERE locations.event = :event')->setParameter('event', $event)->getResult();
         }
     }
 }
