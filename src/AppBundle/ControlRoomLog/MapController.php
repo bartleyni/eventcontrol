fa1e0149c598db690940d7346b7f83fe6b9b9bb2
Nick Bartley <github@nb221.com>
2016-12-29 11:13:02 +0000

    modified geojson data

--- src/AppBundle/ControlRoomLog/MapController.php
+++ src/AppBundle/ControlRoomLog/MapController.php
@@ -241,21 +241,23 @@ class MapController extends Controller
         {
             if($log['latitude'] != null)
             {
-                if (min($log['severity'], $log['medical_severity']) == 1){
+                $severity = min($log['severity'], $log['medical_severity']);
+                
+                if ($severity == 1){
                     $colour = "#E50D00";
-                } elseif(min($log['severity'], $log['medical_severity']) == 2) {
+                } elseif($severity == 2) {
                     $colour = "#DF7200";
-                } elseif(min($log['severity'], $log['medical_severity']) == 3) {
+                } elseif($severity == 3) {
                     $colour = "#D9D100";
-                } elseif(min($log['severity'], $log['medical_severity']) == 4) {
+                } elseif($severity == 4) {
                     $colour = "#7CD300";
-                } elseif(min($log['severity'], $log['medical_severity']) == 5) {
+                } elseif($severity == 5) {
                     $colour = "#1CCE00";
                 } else {
                     $colour = "#ff8080";
                 }
                     
-                $logFeature = ['type' => "Feature", 'properties' => ["marker-color" => $colour, "marker-size" => "medium", "marker-symbol"=> "", "id" => $log['id']], 'geometry' => ["type" => "point", "coordinates" => [floatval($log['longitude']), floatval($log['latitude'])]]];
+                $logFeature = ['type' => "Feature", 'properties' => ["marker-color" => $colour, "marker-size" => "medium", "marker-symbol"=> "", "id" => $log['id'], "severity" => $severity], 'geometry' => ["type" => "point", "coordinates" => [floatval($log['longitude']), floatval($log['latitude'])]]];
                 array_push($data['features'],$logFeature);
             }
         }
