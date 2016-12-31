90954649c3d8c6611d356e1e9bc368f17cbbf101
bunit17 <gitcommits@mattburnett.co.uk>
2016-12-31 23:34:34 +0000

    Adding venue camera form

--- src/AppBundle/ControlRoomLog/VenueController.php
+++ src/AppBundle/ControlRoomLog/VenueController.php
@@ -93,12 +93,10 @@ class VenueController extends Controller
 
         $venue = $em->getRepository('AppBundle\Entity\venue')->findOneBy(array('id' => $id));
         
-        $venue_camera = $em->getRepository('AppBundle\Entity\venue_camera')->findBy(array('venue_id' => $id));
         
-        if(!$venue_camera){
             $venue_camera = new venue_camera();
             $venue_camera->setVenueId($venue);
-        }
+      
 
         $form = $this->createForm(new VenueCameraType(), $venue_camera);
 
