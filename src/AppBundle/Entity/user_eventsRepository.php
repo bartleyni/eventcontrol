7a8fd109e2eabfa1144d446b8417ea4ce5888def
bunit17 <gitcommits@mattburnett.co.uk>
2017-01-01 13:18:48 +0000

    Adding venue camera form

--- src/AppBundle/Entity/user_eventsRepository.php
+++ src/AppBundle/Entity/user_eventsRepository.php
@@ -35,7 +35,7 @@ class user_eventsRepository extends EntityRepository
 
         $user_event = $this->getEntityManager()->createQuery('SELECT IDENTITY(user_event.event_id) FROM AppBundle\Entity\user_events user_event JOIN user_event.event_id event WHERE user_event.User_id = :id AND :nowdate BETWEEN event.event_log_start_date AND event.event_log_stop_date')->setParameter('id', $userId)->setParameter('nowdate', $now)->getOneOrNullResult();
 
-        return $this->getEntityManager()->createQuery('SELECT event_log_stop_date FROM AppBundle\Entity\event event WHERE uevent.event_id = :id')->setParameter('id', $user_event)->getOneOrNullResult();
+        return $this->getEntityManager()->createQuery('SELECT event.event_log_stop_date FROM AppBundle\Entity\event event WHERE uevent.event_id = :id')->setParameter('id', $user_event)->getOneOrNullResult();
 
 
         //return $user_event;
