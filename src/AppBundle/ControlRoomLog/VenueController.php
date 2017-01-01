9d6b4482f93b5d1d315e1a9cfc2466faaba79f08
bunit17 <gitcommits@mattburnett.co.uk>
2017-01-01 00:21:42 +0000

    Addeing add remove to event

--- src/AppBundle/ControlRoomLog/VenueController.php
+++ src/AppBundle/ControlRoomLog/VenueController.php
@@ -93,10 +93,12 @@ class VenueController extends Controller
 
         $venue = $em->getRepository('AppBundle\Entity\venue')->findOneBy(array('id' => $id));
         
+        $venue_camera = $em->getRepository('AppBundle\Entity\venue_camera')->find();
         
+        if(!$venue_camera){
             $venue_camera = new venue_camera();
             $venue_camera->setVenueId($venue);
-      
+        }
 
         $form = $this->createForm(new VenueCameraType(), $venue_camera);
 
