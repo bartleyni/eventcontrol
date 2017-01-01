0680319e3e74d9118ff4f40b87a60c186f108738
bunit17 <gitcommits@mattburnett.co.uk>
2017-01-01 13:37:29 +0000

    Adding venue camera form

--- src/AppBundle/Entity/venue_cameraRepository.php
+++ src/AppBundle/Entity/venue_cameraRepository.php
@@ -6,7 +6,9 @@ class venue_cameraRepository extends EntityRepository
 {
     public function getvenuecameras($id)
     {
-        return $this->getEntityManager()->createQuery('SELECT IDENTITY(p.camera_id) as camera_id, p.inverse FROM AppBundle\Entity\venue_camera p  WHERE p.venue_id = :id')->setParameter('id', $id)->getResult();
+       //('SELECT IDENTITY(user_event.event_id) FROM AppBundle\Entity\user_events user_event JOIN user_event.event_id event WHERE user_event.User_id = :id AND :nowdate BETWEEN event.event_log_start_date AND event.event_log_stop_date')->setParameter('id', $userId)->setParameter('nowdate', $now)->getOneOrNullResult();
+
+        return $this->getEntityManager()->createQuery('SELECT IDENTITY(p.camera_id) as camera_id, p.inverse, camera.name FROM AppBundle\Entity\venue_camera p JOIN p.camera_id camera  WHERE p.venue_id = :id')->setParameter('id', $id)->getResult();
     }
     public function getcameravenue($id)
     {
