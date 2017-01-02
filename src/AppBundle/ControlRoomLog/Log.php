7761e0efca5f6bac19f7d014b0e3ad692b2607e9
Nick Bartley <github@nb221.com>
2017-01-02 13:14:05 +0000

    Removed redundant code

--- src/AppBundle/ControlRoomLog/Log.php
+++ src/AppBundle/ControlRoomLog/Log.php
@@ -227,8 +227,7 @@ class Log extends Controller
         
         $usr = $this->get('security.context')->getToken()->getUser();
         $operatorId = $usr->getId();
-        
-        //$user_event = $em->getRepository('AppBundle\Entity\user_events')->findOneBy(array('User_id' => $operatorId, 'active' => true));
+
         $event = $em->getRepository('AppBundle\Entity\user_events')->getActiveEvent($operatorId);
         $em->flush();
         
@@ -239,167 +238,12 @@ class Log extends Controller
             $eId = $user_event->getEventId();
         }
         
-        //$event = $em->getRepository('AppBundle\Entity\event')->findOneBy(
-            //array('id' => $eId));
-        
         if ($event)
         {
             $eventId = $event->getId();
         } else {
             $eventId = 0;
         }
-        
-        $em->flush();
-        
-        $qb = $em->createQueryBuilder(); 
-        
-        $qb
-            ->select('entry.id, entry.log_entry_open_time, entry.log_update_timestamp, entry.log_blurb, entry.location, entry.reported_by, entry.park_alert, gen.general_description, gen.general_open, gen.general_entry_closed_time, sec.security_description, sec.security_entry_closed_time, secinc.security_incident_description, secinc.severity, secinc.security_incident_colour, med.medical_entry_closed_time, medinj.medical_severity, medinj.medical_injury_description, medrsp.medical_response_description, med.nine_nine_nine_required, lost.lost_property_entry_closed_time, lost.lost_property_description')
-            ->from('AppBundle\Entity\log_entries', 'entry')
-            ->orderBy('entry.log_entry_open_time', $sort_dir)
-            ->leftJoin('AppBundle\Entity\general_log', 'gen', 'WITH', 'gen.log_entry_id = entry.id')
-            ->leftJoin('AppBundle\Entity\security_log', 'sec', 'WITH', 'sec.log_entry_id = entry.id')
-            ->leftJoin('AppBundle\Entity\security_incident_type', 'secinc', 'WITH', 'secinc.id = sec.security_incident_type')
-            ->leftJoin('AppBundle\Entity\medical_log', 'med', 'WITH', 'med.log_entry_id = entry.id')
-            ->leftJoin('AppBundle\Entity\medical_reported_injury_type', 'medinj', 'WITH', 'medinj.id = med.medical_reported_injury_type')
-            ->leftJoin('AppBundle\Entity\medical_response', 'medrsp', 'WITH', 'medrsp.id = med.medical_response')
-            ->leftJoin('AppBundle\Entity\lost_property', 'lost', 'WITH', 'lost.log_entry_id = entry.id')
-            ->leftJoin('AppBundle\Entity\User', 'user', 'WITH', 'user.id = entry.operator')
-            ->leftJoin('AppBundle\Entity\event', 'event', 'WITH', 'event.id = entry.event')
-            ->where('event.id = :eventId')
-            ->setParameter('eventId', $eventId)
-            ;
-        
-        if ($filter_type=="medical"){
-            if ($filter=="open"){
-                $qb->andWhere($qb->expr()->andX(
-                            $qb->expr()->isNotNull('med.medical_description'),
-                            $qb->expr()->isNull('med.medical_entry_closed_time')
-                        ));
-            } elseif ($filter=="closed"){
-                $qb->andWhere($qb->expr()->andX(
-                            $qb->expr()->isNotNull('med.medical_description'),
-                            $qb->expr()->isNotNull('med.medical_entry_closed_time')
-                        ));
-            } else {
-                $qb->andWhere($qb->expr()->isNotNull('med.medical_description'));
-            }
-        } elseif ($filter_type=="security"){
-            if ($filter=="open"){
-                $qb->andWhere($qb->expr()->andX(
-                            $qb->expr()->isNotNull('sec.security_description'),
-                            $qb->expr()->isNull('sec.security_entry_closed_time')
-                        ));
-            } elseif ($filter=="closed"){
-                $qb->andWhere($qb->expr()->andX(
-                            $qb->expr()->isNotNull('sec.security_description'),
-                            $qb->expr()->isNotNull('sec.security_entry_closed_time')
-                        ));
-            } else {
-                $qb->andWhere($qb->expr()->isNotNull('sec.security_description'));
-            }
-        } elseif ($filter_type=="general"){
-            if ($filter=="open"){
-                $qb->andWhere($qb->expr()->andX(
-                            $qb->expr()->isNotNull('gen.general_description'),
-                            $qb->expr()->isNull('gen.general_entry_closed_time')
-                        ) );
-            } elseif ($filter=="closed"){
-                $qb->andWhere($qb->expr()->andX(
-                            $qb->expr()->isNotNull('gen.general_description'),
-                            $qb->expr()->isNotNull('gen.general_entry_closed_time')
-                        ));
-            } else {
-                $qb->andWhere($qb->expr()->isNotNull('gen.general_description'));
-            }
-        } elseif ($filter_type=="lost"){
-            if ($filter=="open"){
-                $qb
-                    ->andWhere($qb->expr()->andX(
-                            $qb->expr()->isNotNull('lost.lost_property_description'),
-                            $qb->expr()->isNull('lost.lost_property_entry_closed_time')
-                        ));
-            } elseif ($filter=="closed"){
-                $qb->andWhere($qb->expr()->andX(
-                           $qb->expr()->isNotNull('lost.lost_property_description'),
-                           $qb->expr()->isNotNull('lost.lost_property_entry_closed_time')
-                        ));
-
-            } else {
-                $qb->andWhere($qb->expr()->isNotNull('lost.lost_property_description'));
-            }
-        }else{
-            if ($filter=="open"){
-                $qb->andWhere($qb->expr()->orX(
-                        $qb->expr()->andX(
-                            $qb->expr()->isNotNull('gen.general_description'),
-                            $qb->expr()->isNull('gen.general_entry_closed_time')
-                             ),                            
-                        $qb->expr()->andX(
-                            $qb->expr()->isNotNull('sec.security_description'),
-                            $qb->expr()->isNull('sec.security_entry_closed_time')
-                             ),
-                        $qb->expr()->andX(
-                            $qb->expr()->isNotNull('med.medical_description'),
-                            $qb->expr()->isNull('med.medical_entry_closed_time')
-                            ),
-                        $qb->expr()->andX(
-                            $qb->expr()->isNotNull('lost.lost_property_description'),
-                            $qb->expr()->isNull('lost.lost_property_entry_closed_time')
-                            )));
-            } elseif ($filter=="closed"){
-                $qb->andWhere($qb->expr()->andX(
-                        $qb->expr()->orX(   
-                            $qb->expr()->isNull('gen.general_description'),
-                            $qb->expr()->andX(
-                                $qb->expr()->isNotNull('gen.general_description'),
-                                $qb->expr()->isNotNull('gen.general_entry_closed_time')
-                                 )
-                            ),
-                        $qb->expr()->orX(   
-                            $qb->expr()->isNull('lost.lost_property_description'),
-                            $qb->expr()->andX(
-                                $qb->expr()->isNotNull('lost.lost_property_description'),
-                                $qb->expr()->isNotNull('lost.lost_property_entry_closed_time')
-                                 )
-                            ),                             
-                        $qb->expr()->orX(   
-                            $qb->expr()->isNull('sec.security_description'),
-                            $qb->expr()->andX(
-                                $qb->expr()->isNotNull('sec.security_description'),
-                                $qb->expr()->isNotNull('sec.security_entry_closed_time')
-                                 )
-                            ),
-                        $qb->expr()->orX(   
-                            $qb->expr()->isNull('med.medical_description'),    
-                            $qb->expr()->andX(
-                                $qb->expr()->isNotNull('med.medical_description'),
-                                $qb->expr()->isNotNull('med.medical_entry_closed_time')
-                                ))));
-            }
-        }
-        
-        if ($event){
-            //$begin = $event->getEventLogStartDate();
-            //$end = $event->getEventLogStopDate();
-            
-            //$qb->andWhere('entry.log_entry_open_time <= :begin')
-                //->andWhere('entry.log_entry_open_time >= :end')
-                //->setParameter('begin', $begin)
-                //->setParameter('end', $end);
-            
-            $eventId = $event->getId();
-            
-            $qb->andWhere('entry.event = :eventId')
-                ->setParameter('eventId', $eventId);
-        }else{
-            $qb->andWhere('entry.log_entry_open_time <= :begin')
-                ->andWhere('entry.log_entry_open_time >= :end')
-                ->setParameter('begin', new \DateTime('2020-04-30'))
-                ->setParameter('end', new \DateTime('2014-04-25'));
-        }
-        $query = $qb->getQuery();
-        //$logs = $query->getResult();
         
         $logs = $em->getRepository('AppBundle\Entity\log_entries')->getLogEntries($eventId, $sort, $filter, $filter_type);
         return $this->render('logTable2.html.twig', array('logs' => $logs));
