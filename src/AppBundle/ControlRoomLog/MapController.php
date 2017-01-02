07d53567a77338cf3a5433ad179a45d50f516020
Nick Bartley <github@nb221.com>
2017-01-02 14:51:59 +0000

    modified marker generation

--- src/AppBundle/ControlRoomLog/MapController.php
+++ src/AppBundle/ControlRoomLog/MapController.php
@@ -103,12 +103,12 @@ class MapController extends Controller
             if($log['latitude'] != null)
             {
                 
-                $key = array_search($log['latitude']+", "+$log['longitude'], $markers);
+                $key = array_search("LL"+$log['latitude']+", "+$log['longitude'], $markers);
                 
                 if($key == null)
                 {
                     $markerId = $markerId+1;
-                    $markers[$markerId] = ['latlong' => $log['latitude']+", "+$log['longitude'], 'latitude' => $log['latitude'], 'longitude' => $log['longitude']];
+                    $markers[$markerId] = ['latlong' => ("LL"+$log['latitude']+", "+$log['longitude']), 'latitude' => $log['latitude'], 'longitude' => $log['longitude']];
                     $key = $markerId;
                 }
                 
