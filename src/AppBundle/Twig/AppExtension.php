65c345ff2334e6b868a19f9c269fbcf4fa105e6d
Nick Bartley <github@nb221.com>
2017-01-02 11:07:02 +0000

    debugging ups

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -61,7 +61,7 @@ class AppExtension extends \Twig_Extension implements \Twig_Extension_GlobalsInt
                 $qb1
                 ->select('ups.id, ups.name, ups.location, event.id, ups.power')
                 ->from('AppBundle\Entity\UPS', 'ups')
-                ->Join('AppBundle\Entity\event', 'event', 'WITH', 'ups.event = event')
+                ->leftJoin('AppBundle\Entity\event', 'event', 'WITH', 'ups.event = event')
                 //->where('event.id = :eventId')
                 //->setParameter('eventId', $eventId)
                 ;
