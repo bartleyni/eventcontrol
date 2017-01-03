a73ae1acf1680db22726b768820cdde7e0dcbe9e
Nick Bartley <github@nb221.com>
2017-01-03 22:40:53 +0000

    debugging active evnet

--- src/AppBundle/Entity/user_eventsRepository.php
+++ src/AppBundle/Entity/user_eventsRepository.php
@@ -21,7 +21,7 @@ class user_eventsRepository extends EntityRepository
     {
         $now = new \DateTime();
         
-        $user_event = $this->getEntityManager()->createQuery('SELECT IDENTITY(user_event.event_id) FROM AppBundle\Entity\user_events user_event JOIN user_event.event_id event WHERE user_event.User_id = :id AND :nowdate BETWEEN event.event_log_start_date AND event.event_log_stop_date')->setParameter('id', $userId)->setParameter('nowdate', $now)->setMaxResults(1)->getResult();
+        $user_event = $this->getEntityManager()->createQuery('SELECT IDENTITY(user_event.event_id) FROM AppBundle\Entity\user_events user_event JOIN user_event.event_id event WHERE user_event.User_id = :id AND :nowdate BETWEEN event.event_log_start_date AND event.event_log_stop_date')->setParameter('id', $userId)->setParameter('nowdate', $now)->setMaxResults(1)->getOneOrNullResult();
         
         return $this->getEntityManager()->getRepository('AppBundle\Entity\event')->findOneBy(array('id' => $user_event));
     }
