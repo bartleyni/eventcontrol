ce9c58dbfd8cba08fbfb8006077dc01980d44e1d
Nick Bartley <github@nb221.com>
2017-01-13 09:14:58 +0000

    Update GeneralType.php

--- src/AppBundle/Form/Type/GeneralType.php
+++ src/AppBundle/Form/Type/GeneralType.php
@@ -12,13 +12,22 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class GeneralType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('general_description', 'textarea', array(
+            ->add('general_description', TextareaType::class, array(
                 'label' => 'General Description',
                 'required' => true,
                 'attr' => array(
@@ -26,14 +35,14 @@ class GeneralType extends AbstractType
                     'rows' => '5'                    
                 )
             ))  
-            ->add('general_open', 'checkbox', array(
+            ->add('general_open', CheckboxType::class, array(
                 'label' => "Incident Open?",
                 'required' => false,
                 'attr' => array(
                     'class' => 'form-control checkbox'
                 )
             ))                
-            ->add('submit_general', 'submit', array(
+            ->add('submit_general', SubmitType::class, array(
                 'label' => 'Submit General',
                 'attr' => array(
                     'formvalidate' => 'formvalidate',
