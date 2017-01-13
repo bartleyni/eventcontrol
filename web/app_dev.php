3d954910c3a5b2e5b0dfdf3a6e19934e32d6024a
Nick Bartley <github@nb221.com>
2017-01-13 00:43:44 +0000

    Updated app for S3

--- web/app_dev.php
+++ web/app_dev.php
@@ -21,7 +21,7 @@ use Symfony\Component\Debug\Debug;
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 Debug::enable();
 
-require_once __DIR__.'/../app/AppKernel.php';
+//require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
