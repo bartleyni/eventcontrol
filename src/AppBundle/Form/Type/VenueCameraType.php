645e8960469acb1ee38766be01d07642321c8317
Nick Bartley <github@nb221.com>
2017-01-13 09:28:10 +0000

    Update VenueCameraType.php

--- src/AppBundle/Form/Type/VenueCameraType.php
+++ src/AppBundle/Form/Type/VenueCameraType.php
@@ -11,7 +11,17 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\IntergerType;
-
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class VenueCameraType extends AbstractType
 {
@@ -19,14 +29,14 @@ class VenueCameraType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('camera_id', 'entity', array(
+            ->add('camera_id', EntityType::class, array(
                 'class' => 'AppBundle:camera',
                 'label' => 'Camera',
                 'attr' => array(
                     'class' => 'form-control checkbox'
                 )
             ))
-            ->add('inverse', 'checkbox', array(
+            ->add('inverse', CheckboxType::class, array(
                 'label' => "Inverse camera",
                 'required' => false,
                 'attr' => array(
