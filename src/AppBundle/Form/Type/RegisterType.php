1a7c41400f51d47d33f9e682956ffbfa0d3955f5
Nick Bartley <github@nb221.com>
2017-01-13 09:20:16 +0000

    Update RegisterType.php

--- src/AppBundle/Form/Type/RegisterType.php
+++ src/AppBundle/Form/Type/RegisterType.php
@@ -10,6 +10,17 @@ namespace AppBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 
 class RegisterType extends AbstractType
@@ -17,9 +28,9 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', 'email')
-            ->add('phone', 'text')
-            ->add('name', 'text')
+            ->add('email', EmailType::class)
+            ->add('phone', TextType::class)
+            ->add('name', TextType::class)
         ;
     }
     public function getName()
