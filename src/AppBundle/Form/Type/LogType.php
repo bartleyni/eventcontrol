8fc1f5436f76803021dcb52cbaa6ee1b60cd13e3
Nick Bartley <github@nb221.com>
2017-01-13 09:01:06 +0000

    Update LogType.php

--- src/AppBundle/Form/Type/LogType.php
+++ src/AppBundle/Form/Type/LogType.php
@@ -18,6 +18,8 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Vich\UploaderBundle\Form\Type\VichFileType;
 
