50190cf98d377fa08164e54874b9fab4e3ab95a9
Nick Bartley <github@nb221.com>
2017-01-13 09:16:23 +0000

    Update LostPropertyType.php

--- src/AppBundle/Form/Type/LostPropertyType.php
+++ src/AppBundle/Form/Type/LostPropertyType.php
@@ -12,27 +12,36 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class LostPropertyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('lost_property_item_lost', 'checkbox', array(
+            ->add('lost_property_item_lost', CheckboxType::class, array(
                 'label' => "Item Lost?",
                 'required' => false,
                 'attr' => array(
                     'class' => 'form-control'
                 )
             ))
-            ->add('lost_property_item_found', 'checkbox', array(
+            ->add('lost_property_item_found', CheckboxType::class, array(
                 'label' => "Item Found?",
                 'required' => false,
                 'attr' => array(
                     'class' => 'form-control'
                 )
             ))
-            ->add('lost_property_description', 'textarea', array(
+            ->add('lost_property_description', TextareaType::class, array(
                 'label' => 'Item Description',
                 'required' => true,
                 'attr' => array(
@@ -40,7 +49,7 @@ class LostPropertyType extends AbstractType
                     'rows' => '3'
                 )
             ))
-            ->add('lost_property_contact_details', 'textarea', array(
+            ->add('lost_property_contact_details', TextareaType::class, array(
                 'label' => 'Contact Details',
                 'required' => false,
                 'attr' => array(
@@ -48,7 +57,7 @@ class LostPropertyType extends AbstractType
                     'rows' => '3'
                 )
             ))                  
-            ->add('lost_property_resolution', 'entity', array(
+            ->add('lost_property_resolution', EntityType::class, array(
                 'class' => 'AppBundle:lost_property_resolution',
                 'label' => 'Lost Property Resolution',
                 'required' => false,
@@ -56,7 +65,7 @@ class LostPropertyType extends AbstractType
                     'class' => 'form-control col-sm-10'
                 )
             ))
-            ->add('lost_property_resolution_description', 'textarea', array(
+            ->add('lost_property_resolution_description', TextareaType::class, array(
                 'label' => 'Lost Property Resolution Notes',
                 'required' => false,
                 'attr' => array(
@@ -64,7 +73,7 @@ class LostPropertyType extends AbstractType
                     'rows' => '3'
                 )
             ))              
-            ->add('submit_lost_property', 'submit', array(
+            ->add('submit_lost_property', SubmitType::class, array(
                 'label' => 'Submit Lost Property',
                 'attr' => array(
                     'formvalidate' => 'formvalidate',
