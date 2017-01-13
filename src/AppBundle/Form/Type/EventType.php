7ad07e81b1def7b1f3ea6fde45fb00bec6756fc0
Nick Bartley <github@nb221.com>
2017-01-13 09:43:56 +0000

    Update EventType.php

--- src/AppBundle/Form/Type/EventType.php
+++ src/AppBundle/Form/Type/EventType.php
@@ -11,7 +11,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
