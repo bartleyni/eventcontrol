53642902c02cdfc16e3650cb504062d5d1b19c30
bunit17 <gitcommits@mattburnett.co.uk>
2017-01-22 15:15:06 +0000

    Edditing doors to be a propertie of venue_event

--- src/AppBundle/ControlRoomLog/VenueController.php
+++ src/AppBundle/ControlRoomLog/VenueController.php
@@ -123,6 +123,8 @@ class VenueController extends Controller
         $venue_event = $em->getRepository('AppBundle\Entity\venue')->getactiveeventvenues($usr);
 
         foreach ($venue_event as $key => $value) {
+            $venues[$key]['id'] = $value['venue_id']['id'];
+            $venues[$key]['name'] = $value['venue_id']['name'];
             $venues[$key]['count'] = $em->getRepository('AppBundle\Entity\venue')->getvenuecount($value['venue_id']['id'], $value['event_id']['event_log_stop_date'], $value['doors']);
             $status = $em->getRepository('AppBundle\Entity\venue')->getvenuestatus($value['id']);
             if ($status) {   $venues[$key]['status'] = "true"; }else{  $venues[$key]['status'] = "false"; }
