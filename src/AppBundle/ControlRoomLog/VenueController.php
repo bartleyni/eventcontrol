3667f3b80efdb393ab4f2d121c895da13056142e
bunit17 <gitcommits@mattburnett.co.uk>
2017-05-21 22:00:13 +0100

    Updating venue status

--- src/AppBundle/ControlRoomLog/VenueController.php
+++ src/AppBundle/ControlRoomLog/VenueController.php
@@ -123,7 +123,7 @@ class VenueController extends Controller
             $venues[$key]['id'] = $value['venue_id']['id'];
             $venues[$key]['name'] = $value['venue_id']['name'];
             $venues[$key]['count'] = $em->getRepository('AppBundle\Entity\venue')->getvenuecount($value['venue_id']['id'], $value['event_id']['event_log_stop_date'], $value['doors']);
-            $status = $em->getRepository('AppBundle\Entity\venue')->getvenuestatus($value['venue_id']);
+            $status = $em->getRepository('AppBundle\Entity\venue')->getvenuestatus($value['venue_id']['id']);
             if ($status) {   $venues[$key]['status'] = "true"; }else{  $venues[$key]['status'] = "false"; }
             $status = $em->getRepository('AppBundle\Entity\venue')->getpeoplecountingstatus();
             if ($status) {   $venues['people_counting_status'] = "true"; }else{  $venues['people_counting_status'] = "false"; }
