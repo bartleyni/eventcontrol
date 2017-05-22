73be091f3a3d14b9971428e8cb14578d4bf692d6
Nick Bartley <github@nb221.com>
2017-05-22 23:22:15 +0100

    Created get venue counts by event id

--- src/AppBundle/ControlRoomLog/VenueController.php
+++ src/AppBundle/ControlRoomLog/VenueController.php
@@ -143,9 +143,42 @@ class VenueController extends Controller
         }
 
         return $response;
+    }
+    
+    /**
+     * @Route("/venue/event/{id}", name="venue_event_json_data");
+     *
+     */
+    public function venue_event_json_data()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $usr = $this->get('security.token_storage')->getToken()->getUser();
+        $venue_event = $em->getRepository('AppBundle\Entity\venue')->getEventVenues($id);
 
+        foreach ($venue_event as $key => $value) {
+            $venues[$key]['id'] = $value['venue_id']['id'];
+            $venues[$key]['name'] = $value['venue_id']['name'];
+            $venues[$key]['count'] = $em->getRepository('AppBundle\Entity\venue')->getvenuecount($value['venue_id']['id'], $value['event_id']['event_log_stop_date'], $value['doors']);
+            $status = $em->getRepository('AppBundle\Entity\venue')->getvenuestatus($value['venue_id']['id']);
+            if ($status) {   $venues[$key]['status'] = "true"; }else{  $venues[$key]['status'] = "false"; }
+            $status = $em->getRepository('AppBundle\Entity\venue')->getpeoplecountingstatus();
+            if ($status) {   $venues['people_counting_status'] = "true"; }else{  $venues['people_counting_status'] = "false"; }
+        }
 
-        //return $response;
+        if ($venues)
+        {
+            $response = new JsonResponse();
+            $response->setData($venues);
+
+        } else {
+            $response = new Response();
+            $response->setContent('Hello World');
+            $response->headers->set('Content-Type', 'text/plain');
+            $response->setStatusCode(Response::HTTP_NOT_FOUND);
+            $response->send();
+        }
+
+        return $response;
     }
 
     /**
