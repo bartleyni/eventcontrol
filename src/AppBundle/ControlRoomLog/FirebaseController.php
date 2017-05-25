a2f2abab3c5b375f892bff9f4d9148c5b5bb4f6f
Nick Bartley <github@nb221.com>
2017-05-25 19:06:44 +0100

    debugging firebase registration

--- src/AppBundle/ControlRoomLog/FirebaseController.php
+++ src/AppBundle/ControlRoomLog/FirebaseController.php
@@ -13,6 +13,7 @@ use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Description of FirebaseController
