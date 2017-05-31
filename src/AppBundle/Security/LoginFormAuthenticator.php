78e1b675a7d94418712761ed8ff48df058d4392e
Nick Bartley <github@nb221.com>
2017-05-31 08:22:35 +0100

    Update LoginFormAuthenticator.php

--- src/AppBundle/Security/LoginFormAuthenticator.php
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -24,6 +24,7 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Form\FormFactoryInterface;
 use Doctrine\ORM\EntityManager;
+use AppBundle\Form\Type\LoginForm;
 
 
 /**
