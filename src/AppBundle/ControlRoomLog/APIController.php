605e89a4a2686c4f48ea34443abc1328ccc7fa6e
Nick Bartley <github@nb221.com>
2017-08-21 20:16:07 +0100

    Added json encoding to api

--- src/AppBundle/ControlRoomLog/APIController.php
+++ src/AppBundle/ControlRoomLog/APIController.php
@@ -9,16 +9,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\UploadFile;
-use Symfony\Component\HttpFoundation\File\File;
-use AppBundle\Form\Type\EventType;
-use AppBundle\Entity\event;
-use AppBundle\Entity\Alert;
-use AppBundle\Entity\Queue;
-use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class APIController extends Controller
 {
+    
     /**
     * @Route("/api/events", name="api_events")
      * 
@@ -29,9 +27,14 @@ class APIController extends Controller
     
     public function apiEventListAction(Request $request)
     {
+        $encoders = array(new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+
+        $serializer = new Serializer($normalizers, $encoders);
+        
         $em = $this->getDoctrine()->getManager();
         $usr = $this->get('security.token_storage')->getToken()->getUser();
-        $selected_event = $usr;
+        $selected_event = $jsonContent = $serializer->serialize($usr->getSelectedEvent(), 'json');
         $active_events = $em->getRepository('AppBundle\Entity\User')->getActiveEvents($usr);
 
         if ($selected_event)
