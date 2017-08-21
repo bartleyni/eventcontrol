08831b3c135fdfdc5a47d9f5f7f959df805c3c79
Nick Bartley <github@nb221.com>
2017-08-21 20:20:36 +0100

    Debugging api json response

--- src/AppBundle/ControlRoomLog/APIController.php
+++ src/AppBundle/ControlRoomLog/APIController.php
@@ -34,13 +34,13 @@ class APIController extends Controller
         
         $em = $this->getDoctrine()->getManager();
         $usr = $this->get('security.token_storage')->getToken()->getUser();
-        $selected_event = $serializer->serialize($usr->getSelectedEvent(), 'json');
+        $selected_event = $usr->getSelectedEvent();
         $active_events = $em->getRepository('AppBundle\Entity\User')->getActiveEvents($usr);
 
         if ($selected_event)
         {
                 $response = new JsonResponse();
-                $response->setData($selected_event);
+                $response->setData($active_events);
 
         } else {
             $response->setContent('No Events');
