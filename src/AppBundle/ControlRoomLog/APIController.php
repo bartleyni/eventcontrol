27c55881da7dfdbff23b043eee4acab0f0ee8852
Nick Bartley <github@nb221.com>
2017-08-21 20:08:57 +0100

    debugging api

--- src/AppBundle/ControlRoomLog/APIController.php
+++ src/AppBundle/ControlRoomLog/APIController.php
@@ -31,7 +31,7 @@ class APIController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $usr = $this->get('security.token_storage')->getToken()->getUser();
-        $selected_event = $usr->getSelectedEvent();
+        $selected_event = $usr;
         $active_events = $em->getRepository('AppBundle\Entity\User')->getActiveEvents($usr);
 
         if ($selected_event)
