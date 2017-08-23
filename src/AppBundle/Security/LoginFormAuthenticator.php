c2e02f5a9d6d5c1ba0b4f96f029306845876b10a
Nick Bartley <github@nb221.com>
2017-08-23 23:30:13 +0100

    Modified standard login method

--- src/AppBundle/Security/LoginFormAuthenticator.php
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -105,7 +105,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
     
     protected function getDefaultSuccessRedirectUrl()
     {
-        return $this->router->generate('/');
+        return $this->router->generate('/log');
     }
     
     public function supportsRememberMe()
