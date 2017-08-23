5a8fd8efc32107951c6417b218de0964c86aefc3
Nick Bartley <github@nb221.com>
2017-08-23 19:16:35 +0100

    Modified Api auth method

--- src/AppBundle/Security/ApiTokenAuthenticator.php
+++ src/AppBundle/Security/ApiTokenAuthenticator.php
@@ -25,76 +25,79 @@ use Symfony\Component\Routing\RouterInterface;
  *
  * @author Nick
  */
-class ApiTokenAuthenticator extends AbstractGuardAuthenticator 
+
+class ApiTokenAuthenticator extends AbstractGuardAuthenticator
 {
-    private $em;
-    private $router;
-    
-    public function __construct(EntityManager $em, RouterInterface $router)
+   public function getCredentials(Request $request)
     {
-        $this->em = $em;
-        $this->router = $router;
-    }
-    
-    public function getCredentials(Request $request)
-    {
-        if(!$token = $request->headers->get('X-AUTH-TOKEN'))
-        {
+        if (!$token = $request->headers->get('X-AUTH-TOKEN')) {
+            // No token?
             $token = null;
         }
+
+        // What you return here will be passed to getUser() as $credentials
         return array(
             'token' => $token,
         );
     }
+
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        $user = $this->em->getRepository('AppBundle:User')
-            ->findOneBy(array('apiToken' => $credentials['token']));
-        // we could just return null, but this allows us to control the message a bit more
-        if (!$user) {
-            throw new AuthenticationCredentialsNotFoundException();
+        $apiKey = $credentials['token'];
+
+        if (null === $apiKey) {
+            return;
         }
         
-//        $apiKey = $credentials['token'];
-//
-//        if (null === $apiKey) {
-//            return;
-//        }
+        $user = $this->em->getRepository('AppBundle\Entity\User')->findOneBy(['apiToken' => $apiKey]);
 
         // if a User object, checkCredentials() is called
         //return $userProvider->loadUserByUsername($apiKey);
         return $user;
     }
+
     public function checkCredentials($credentials, UserInterface $user)
     {
-        // the fact that they had a valid token that *was* attached to a user
-        // means that their credentials are correct. So, there's nothing
-        // additional (like a password) to check here.
-        return;
+        // check credentials - e.g. make sure the password is valid
+        // no credential check is needed in this case
+
+        // return true to cause authentication success
+        return true;
     }
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
-    {
-        return new JsonResponse(
-            // you could translate the message
-            array('message' => $exception->getMessageKey(), 'message2' => 'Authenticaion Failure'),
-            403
-        );
-    }
+
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
-        // do nothing - let the request just continue!
-        return;
+        // on success, let the request continue
+        return null;
     }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        $data = array(
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+
+            // or to translate this message
+            // $this->translator->trans($exception->getMessageKey(), $exception->getMessageData())
+        );
+
+        return new JsonResponse($data, Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * Called when authentication is needed, but it's not sent
+     */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        $data = array(
+            // you might translate this message
+            'message' => 'Authentication Required'
+        );
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
     public function supportsRememberMe()
     {
         return false;
-    }
-    public function start(Request $request, AuthenticationException $authException = null)
-    {
-        return new JsonResponse(
-            // you could translate the message
-            array('message' => 'Authentication required'),
-            Response::HTTP_UNAUTHORIZED
-        );
-    }
+    } 
 }
