1d6c74f657061d1ca69a5707a316ea4a43f2da87
Nick Bartley <github@nb221.com>
2017-08-30 08:13:27 +0100

    Update app.php

--- web/app.php
+++ web/app.php
@@ -10,11 +10,11 @@ $loader = require __DIR__.'/../app/autoload.php';
 // You should change the ApcClassLoader first argument to a unique prefix
 // in order to prevent cache key conflicts with other applications
 // also using APC.
-/*
+
 $apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
-$loader->unregister();
+//$loader->unregister();
 $apcLoader->register(true);
-*/
+
 
 //require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
