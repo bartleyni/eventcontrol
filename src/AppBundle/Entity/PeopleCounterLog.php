accd4759dd5f2cab91e38d9fa8f741575afd92ca
Nick Bartley <github2@nb221.com>
2017-09-03 23:29:36 +0100

    added aditional functions

--- src/AppBundle/Entity/PeopleCounterLog.php
+++ src/AppBundle/Entity/PeopleCounterLog.php
@@ -91,5 +91,101 @@ class PeopleCounterLog {
     {
         return $this->timestamp;
     }
+    
+    /**
+     * Set event
+     *
+     * @param \AppBundle\Entity\event $event
+     *
+     * @return PeopleCounterLog
+     */
+    public function setEvent(\AppBundle\Entity\event $event = null)
+    {
+        $this->event = $event;
+
+        return $this;
+    }
+    
+    /**
+     * Get event
+     *
+     * @return \AppBundle\Entity\event
+     */
+    public function getEvent()
+    {
+        return $this->event;
+    }
+
+    /**
+     * Get venue
+     *
+     * @return \AppBundle\Entity\venue
+     */
+    public function getVenue()
+    {
+        return $this->venue;
+    }
+    
+    /**
+     * Set venue
+     *
+     * @param \AppBundle\Entity\venue $venue
+     *
+     * @return PeopleCounterLog
+     */
+    public function setVenue(\AppBundle\Entity\venue $venue = null)
+    {
+        $this->venue = $venue;
+
+        return $this;
+    }
+    
+    /**
+     * Set running_in
+     *
+     * @param integer $running_in
+     *
+     * @return PeopleCounterLog
+     */
+    public function setRunningIn($running_in)
+    {
+        $this->running_in = $running_in;
+
+        return $this;
+    }
+
+    /**
+     * Get running_in
+     *
+     * @return integer
+     */
+    public function getRunningIn()
+    {
+        return $this->running_in;
+    }
+
+    /**
+     * Set running_out
+     *
+     * @param integer $running_out
+     *
+     * @return PeopleCounterLog
+     */
+    public function setRunningOut($running_out)
+    {
+        $this->running_out = $running_out;
+
+        return $this;
+    }
+
+    /**
+     * Get running_out
+     *
+     * @return integer
+     */
+    public function getRunningOut()
+    {
+        return $this->running_out;
+    }
 
 }
