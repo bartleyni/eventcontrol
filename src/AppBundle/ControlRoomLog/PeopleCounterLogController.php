4e838a2ac2a5ea6a7e5fed7d1b5f09754b9dee8f
Nick Bartley <github2@nb221.com>
2017-09-04 09:25:28 +0100

    Modified chart data

--- src/AppBundle/ControlRoomLog/PeopleCounterLogController.php
+++ src/AppBundle/ControlRoomLog/PeopleCounterLogController.php
@@ -32,25 +32,53 @@ class PeopleCounterLogController extends Controller
         }
         
         $em->flush();
-
+        
+        $venues = $em->getRepository('AppBundle\Entity\venue')->getEventVenues($eventId);
+        
+        $series = [];
+        
+        foreach ($venues as $venue) {
+            
+            $data = [];
+           
+            $counts = $em->getRepository('AppBundle\Entity\PeopleCounterLog')->findBy(array('event' => $event, 'venue' => $venue));
+            
+            foreach ($counts as $count){
+                array_push($data,array($count->getTimestamp(),$count->getRunningIn() - $count->getRunningOut()));
+            }
+            
+            $venue_count = array("name" => $venue['name'], "data" => $data);
+            
+            if ($data){
+                array_push($series, $venue_count);
+            }
+            
+        }
 
         // Chart
-        $series = array(
-            array("name" => "Data Serie Name",    "data" => array(1,2,4,5,6,3,8))
-        );
+//        $series = array(
+//            array("name" => "Data Serie Name",    "data" => array(1,2,4,5,6,3,8))
+//        );
+        
+        if($series)
+        {
+        
 
-        $ob = new Highchart();
-        $ob->chart->renderTo('linechart');  // The #id of the div where to render the chart
-        $ob->title->text($event->getName().' - Venue Occupancy');
-        $ob->chart->zoomType('x');
-        $ob->xAxis->title(array('text'  => "Time"));
-        $ob->xAxis->type('datetime');
-        $ob->yAxis->title(array('text'  => "Total Number of People"));
-        $ob->series($series);
+            $ob = new Highchart();
+            $ob->chart->renderTo('linechart');  // The #id of the div where to render the chart
+            $ob->title->text($event->getName().' - Venue Occupancy');
+            $ob->chart->zoomType('x');
+            $ob->xAxis->title(array('text'  => "Time"));
+            $ob->xAxis->type('datetime');
+            $ob->yAxis->title(array('text'  => "Total Number of People"));
+            $ob->series($series);
 
-        return $this->render('::peopleCountingLog.html.twig', array(
-            'chart' => $ob
-        ));
+            return $this->render('::peopleCountingLog.html.twig', array(
+                'chart' => $ob
+            ));
+        } else {
+            return $this->render('::peopleCountingLog.html.twig');
+        }
     }
 
 }
