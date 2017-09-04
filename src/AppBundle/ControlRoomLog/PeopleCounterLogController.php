4ce12b4c31b2aabf9b49c3eebd57ec8442cf76b7
Nick Bartley <github2@nb221.com>
2017-09-04 21:58:31 +0100

    modified extremes in graph

--- src/AppBundle/ControlRoomLog/PeopleCounterLogController.php
+++ src/AppBundle/ControlRoomLog/PeopleCounterLogController.php
@@ -77,7 +77,9 @@ class PeopleCounterLogController extends Controller
             $ob->chart->zoomType('x');
             $ob->xAxis->title(array('text'  => "Time(UTC)"));
             $ob->xAxis->type('datetime');
-            $ob->xAxis->setExtremes((time()-8*60*60)*1000,time()*1000);
+            $xMin = (time()-8*60*60)*1000;
+            $xMax = time()*1000;
+            $ob->xAxis->setExtremes($xMin,$xMax);
             $ob->yAxis->title(array('text'  => "Total Number of People"));
             $ob->series($series);
 
