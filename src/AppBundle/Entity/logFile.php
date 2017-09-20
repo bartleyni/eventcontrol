70b1a2767b6474a091e9d97159b399394b9b392b
Nick Bartley <116109+bartleyni@users.noreply.github.com>
2017-09-20 09:06:30 +0100

    Update logFile.php

--- src/AppBundle/Entity/logFile.php
+++ src/AppBundle/Entity/logFile.php
@@ -6,10 +6,13 @@ use Symfony\Component\Security\Core\Role\RoleInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
 
 /**
  * @ORM\Table(name="logFiles")
  * @ORM\Entity()
+ * @Vich\Uploadable
  * @ORM\HasLifecycleCallbacks
  */
 class logFile
@@ -20,20 +23,25 @@ class logFile
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
-         
-         
+        
     /**
-     * Image path
+     * @ORM\Column(type="string", length=255, nullable=false)
      *
      * @var string
-     *
-     * @ORM\Column(type="text", length=255, nullable=false)
      */
-    protected $path;
+    private $fileName;
 
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     *
+     * @var \DateTime
+     */
+    private $updatedAt;
+    
     /**
      * Image file
      *
+     * @Vich\UploadableField(mapping="log_support_file", fileNameProperty="log_supportFileName")
      * @var File
      * @Assert\File(
      *     maxSize = "50M",
@@ -45,7 +53,7 @@ class logFile
     protected $file;
 
     /**
-     * @ORM\ManyToOne(targetEntity="log_entries")
+     * @ORM\ManyToOne(targetEntity="log_entries", inversedBy="attachments")
      * @ORM\JoinColumn(name="log_id", referencedColumnName="id")
      */
     
@@ -59,6 +67,30 @@ class logFile
     public function getFile()
     {
         return $this->file;
+    }
+    
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $file
+     *
+     * @return event
+     */
+    public function setFile(File $file = null)
+    {
+        $this->file = $file;
+
+        if ($file) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+
+        return $this;
     }
     
     /**
@@ -116,30 +148,56 @@ class logFile
     {
         return $this->id;
     }
-
+    
     /**
-     * Set path
+     * set File Name
+     * @param string $fileName
      *
-     * @param string $path
-     *
-     * @return logFile
+     * @return logfile
      */
-    public function setPath($path)
+    public function setFileName($fileName)
     {
-        $this->path = $path;
+        $this->fileName = $fileName;
 
         return $this;
     }
 
     /**
-     * Get path
+     * Get File Name
      *
-     * @return string
+     * @return string|null
      */
-    public function getPath()
+    public function getFileName()
     {
-        return $this->path;
+        return $this->fileName;
     }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return logFile
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
+
+
 
     /**
      * Set logEntry
