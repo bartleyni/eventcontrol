d767eab69eeda2be5dc043f44e238bfd09d264df
Nick Bartley <116109+bartleyni@users.noreply.github.com>
2017-09-20 11:05:42 +0100

    Update LogType.php

--- src/AppBundle/Form/Type/LogType.php
+++ src/AppBundle/Form/Type/LogType.php
@@ -90,12 +90,13 @@ class LogType extends AbstractType
                     'class' => 'form-control'
                 )
             ))
+            /*
             ->add('logFiles', CollectionType::class, array(
                 'entry_type' => LogFileType::class,
                 'allow_add' => true,
                 'allow_delete' => true,
                 'by_reference' => false,
-            ))
+            ))*/
             ->add('submit', SubmitType::class, array(
                 'attr' => array(
                     'formvalidate' => 'formvalidate',
